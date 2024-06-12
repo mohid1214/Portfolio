376a55a7d0403c86789bfa1bd5470140f7963476
mohid1214 <sumyalmuhammad@gmail.com>
2024-06-12 16:49:43 +0500

    ..

---
+++ Pizza Run/CashierCentralizationScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CashierCentralizationScript : MonoBehaviour
+{
+    public OrdersInList orderInListScript;
+    public CashierTakeOrder cashierToTakeOrderScript;
+    public CashierPrepareOrder cashierPrepareOrderScript;
+    public CashierDeliverOrder cashierDeliverOrderScript;
+}
