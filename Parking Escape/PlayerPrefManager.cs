961b5220ccdd5483011608c1d6afcee2cfeb9942
mohid1214 <sumyalmuhammad@gmail.com>
2024-07-10 16:39:19 +0500

    ..

--- Parking Escape/PlayerPrefManager.cs
+++
@@ -1,8 +0,0 @@
-
-using UnityEngine;
-
-public static  class PlayerPrefManager 
-{
-    public static string currentlevel = "currentLevel";
-    public static string TotalConins = "totalcoins";
-}
--- SuperMarketSimulator/EnumDisplay.cs
+++
@@ -1,8 +0,0 @@
-
-
-public enum Display
-{
-    shelf,
-    fridge,
-    freezer
-}
